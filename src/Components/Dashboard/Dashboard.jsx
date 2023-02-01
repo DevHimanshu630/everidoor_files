f35d05c99ac72d018a9f5af93585385e21e14b2e
everidoor <himanshu@molog.in>
2023-02-01 14:52:18 +0530

    display popup set

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
-import styles from "./Dashboard.module.css"
+import styles from "./dashboard.module.css"
 import img from "../Images/img.png"
 export default function Dashboard() {
+
+    const [btn,setbtn]=useState(false)
+    const handledisplay=()=>{
+        setbtn(!btn);
+    }
   return (
     // {styles.bannerContainer}  
     // {`${styles.btndisplay} ${styles.blue}`}
@@ -26,7 +31,7 @@ export default function Dashboard() {
                         <span className={styles.span_content}>Display Dashboard</span>
                     </div>
                     <div className={styles.button_cont}>
-                        <button className={`${styles.btndisplay} ${styles.blue}`}>+ ADD A DISPLAY</button>
+                        <button className={`${styles.btndisplay} ${styles.blue}`} onClick={handledisplay} id="adddisplay">+ ADD A DISPLAY</button>
                         <button className={`${styles.btndisplay} ${styles.brown}`}>Manage Displays</button>
                     </div>
                     
@@ -50,7 +55,29 @@ export default function Dashboard() {
                 </div>
             {/* display popup */}
 
+            <div className={styles.popup} style={{display:btn?"flex":"none"}}>
+            <div className={styles.contentcontainer}>
+        <div className={`${styles.mainheadingcontent} ${styles.flex}`}>
+            <p className={`${styles.mainheadingcontent_para} ${styles.flex}`}>Add a display</p>
+        </div>
+         <form action="" style={{marginTop: '40px'}}>
+             
+            <label for="" className={styles.screencodecontent}>Screen code</label><br />
+            <input type="text" className={styles.screentext} placeholder="Enter Screen Code"/><br />
+            <label for="" className={styles.screencodecontent}>Screen Name</label><br />
+            <input type="text" className={styles.screentext} placeholder="Enter Screen Name"/><br />
+            <label for="" className={styles.screencodecontent}>Time Zone</label><br />
+            <input type="text" className={styles.screentext} placeholder="(GMT + 5:30) Bombay Culcutta, Madras, New Delhi"/>
+        
+        </form>
 
+
+        <div className={`${styles.btncontainer} ${styles.flex}`}>
+            <button className={styles.addbtn} id="Addbtn">Add</button>
+            <button className={styles.cancelbtn} onClick={handledisplay}>Cancel</button>
+        </div>
+    </div>
+            </div>
 
 
 
